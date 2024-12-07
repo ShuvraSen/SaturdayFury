c6f43168920f4a86cad81b959d9c641f6f142e3d
ShuvraSen <shuvra.sen.it@gmail.com>
2024-12-07 17:04:28 -0600

    updated develop

--- src/com/upSkill/java_1/DataTypes.java
+++ src/com/upSkill/java_1/DataTypes.java
@@ -4,6 +4,7 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		System.out.println("i aim to get a job in next year ");
+		System.out.println("make ur level better  ");
 
 	}
 
